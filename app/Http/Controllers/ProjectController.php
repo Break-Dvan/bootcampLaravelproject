935aa7f6b83be6668b35a3d8cd9b00edab61e5f2
Break-Dvan <login@mailvanbraak.nl>
2023-09-20 10:32:45 +0200

    First Commit this Project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -35,11 +35,12 @@ class ProjectController extends Controller
             'name' => 'required|max:120',
             'description' => 'required'
         ]);
-        Project::create([
-            'name' => $request->name,
-            'description' => $request->description,
-        ]);
-        return to_route('projects.index');
+//        Project::create([
+//            'name' => $request->name,
+//            'description' => $request->description,
+//        ]);
+//        return to_route('projects.index');
+        dd($request);
     }
 
 
